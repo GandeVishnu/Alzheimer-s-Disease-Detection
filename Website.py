7f998f3559f4055a694d60956dfd817a55be16f2
Gande Vishnu Vardhan <gandevishnu2002@gmail.com>
2025-04-28 20:28:32 +0530

    Update Website.py

--- Website.py
+++ Website.py
@@ -21,7 +21,7 @@ applications_collection = db["applications"]   # Application form collection
 # -------------------- Constants --------------------
 page_title = "Alzheimers Disease Detection"
 page_icon = "🧠"
-MODEL_PATH = r"F:\ADNI_5_FINAL_FOLDER\20_04_2025_ADNI_best_model.keras"
+MODEL_PATH = "20_04_2025_ADNI_best_model.keras"
 IMG_SIZE = (224, 224)
 class_labels = ['Final AD JPEG', 'Final CN JPEG', 'Final EMCI JPEG', 'Final LMCI JPEG', 'Final MCI JPEG']
 
